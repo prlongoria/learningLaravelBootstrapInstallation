0bce23202a9719a76be3d65637da45ff47947e1f
Patricia Rodríguez <prlongoria@gmail.com>
2022-08-25 05:57:57 +0200

    test create passed, 1 example test not ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         Event::factory()->create(['name'=>'Evento Real', 'description'=>'Esto es un evento que meto de prueba', 'image'=>'https://th.bing.com/th/id/OIP.-n_BoO-Kla10Euys8wXOxgHaFj?pid=ImgDet&rs=1', 'spaces'=>'7']);
-        // Event::factory(10)->create();
+        Event::factory(10)->create();
         // Event::factory()->create(['name'=>'Admin', 'email'=>'admin@mail.com']);
         // Event::factory()->create(['name'=>'User1', 'email'=>'user1@mail.com']);
         // Event::factory()->create(['name'=>'User2', 'email'=>'user2@mail.com']);
--- tests/Feature/CrudTest.php
+++ tests/Feature/CrudTest.php
@@ -9,25 +9,17 @@ use Tests\TestCase;
 
 class CrudTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */use RefreshDatabase;
-    // public function test_example()
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(500);
-    // }
+    
+    use RefreshDatabase;
+    
     public function test_list_event_appear_in_homepage() {
         $this-> withExceptionHandling();
         $events = Event::factory(2)->create(); 
         $event = $events[0];
-        $response = $this -> get('/');
+        $response = $this->get('/');
         $response -> assertStatus(200) 
-        -> assertViewIs('home');
-        $response -> assertSee($event->name);
+            ->assertViewIs('home');
+        $response->assertSee($event->name);
     }
     public function test_a_event_can_ba_deleted() {
         $this-> withExceptionHandling();
@@ -35,5 +27,15 @@ class CrudTest extends TestCase
         $this->assertCount(1, Event::all());
         $response = $this->delete(route('delete', $event->id));
         $this->assertCount(0, Event::all());
+    }
+    public function create() {
+        $this-> withExceptionHandling();
+        $response = $this->post(route('storeEvent'), [
+            'name' => 'new name',
+            'description' => 'new description',
+            'spaces' => '20',
+            'image' => 'new image'
+        ]);
+        $this->assertCount(1, Event::all());
     }
 }
